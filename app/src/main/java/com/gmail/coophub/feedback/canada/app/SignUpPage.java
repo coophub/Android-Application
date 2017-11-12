393b90790d1e19f3541f0664e64f96650d229ea2
Benoît Jeaurond <bensonj@hotmail.ca>
2017-11-12 12:14:25 -0500

    Removed email restriction

--- app/src/main/java/com/gmail/coophub/feedback/canada/app/SignUpPage.java
+++ app/src/main/java/com/gmail/coophub/feedback/canada/app/SignUpPage.java
@@ -9,10 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.regex.Pattern;
 
 public class SignUpPage extends AppCompatActivity {
 
     Typeface tf, tf2;
+    EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +39,13 @@ public class SignUpPage extends AppCompatActivity {
         TextView tv1 = (TextView) findViewById(R.id.signUpDescription);
         tv1.setTypeface(tf);
 
-
         TextView tv2 = (TextView) findViewById(R.id.signUpMainTitle);
         tv2.setTypeface(tf2);
 
         EditText et1 = (EditText) findViewById(R.id.editText2);
         et1.setTypeface(tf);
 
-        EditText et2 = (EditText) findViewById(R.id.editText3);
+        et2 = (EditText) findViewById(R.id.editText3);
         et2.setTypeface(tf);
 
         EditText et3 = (EditText) findViewById(R.id.firstName);
@@ -58,8 +59,47 @@ public class SignUpPage extends AppCompatActivity {
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (checkEmail(String.valueOf(et2.getText()))){
+                }
                 startActivity(new Intent(SignUpPage.this, ConnectPage.class));
             }
         });
+    }
+
+    public void checkPassword(String password){
+
+        // Check if there's a digit
+        String regDigit = "(?=.*\\d)";
+        Pattern pattern = Pattern.compile(regDigit);
+        Boolean boolDigit = pattern.matcher(password).matches();
+
+        // Check if there's a lowercase character
+        String regLower = "(?=.*[a-z])";
+        pattern = Pattern.compile(regLower);
+        Boolean boolLower = pattern.matcher(password).matches();
+
+        // Check if there's a uppercase character
+        String regUpper = "(?=.*[A-Z])";
+        pattern = Pattern.compile(regUpper);
+        Boolean boolUpper = pattern.matcher(password).matches();
+
+        // Check if it contains @, #, $ or %
+        String regSpecial = "(?=.*[@#$%])";
+        pattern = Pattern.compile(regSpecial);
+        Boolean boolSpecial = pattern.matcher(password).matches();
+
+        // Check if it's between 6 and 20 characters
+        String regLength = ".{6,20}";
+        pattern = Pattern.compile(regLength);
+        Boolean boolLength = pattern.matcher(password).matches();
+
+
+    }
+
+    public Boolean checkEmail(String email){
+        String regEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(regEmail);
+        Boolean boolEmail = pattern.matcher(email).matches();
+        return boolEmail;
     }
 }
